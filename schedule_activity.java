76b21ac182f78f0924e00f1eea99cdc202a970dd
Mai Bui <83102564+MaiBui-code@users.noreply.github.com>
2022-10-21 00:46:35 -0400

    Add files via upload

---
+++ schedule_activity.java
@@ -0,0 +1,201 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		schedule
+	 *	@date 		Monday 10th of October 2022 08:32:53 PM
+	 *	@title 		Sleep (Screens)
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.content.Intent;
+
+public class schedule_activity extends Activity {
+
+	
+	private View _bg__schedule_ek2;
+	private ImageView image_bg;
+	private ImageView schedule_box;
+	private TextView _10_30am;
+	private TextView _10_30am_ek1;
+	private ImageView _307692789_491831092502669_2658396862767040473_n_1;
+	private TextView question_button;
+	private TextView add_button;
+	private View handler_1;
+	private View wrap_Monday;
+	private TextView s;
+	private View rectangle_25_ek1;
+	private View rectangle_25_ek2;
+	private TextView s_ek1;
+	private TextView s_ek2;
+	private TextView s_ek3;
+	private TextView _10_30am_ek2;
+	private TextView s_ek4;
+	private TextView s_ek5;
+	private View ellipse_12;
+	private View ellipse_12_ek1;
+	private View on_off_1;
+	private View rectangle_24_ek2;
+	private View rectangle_25_ek4;
+	private TextView s_ek6;
+	private View rectangle_25_ek5;
+	private TextView s_ek7;
+	private View rectangle_25_ek6;
+	private View rectangle_25_ek7;
+	private TextView s_ek8;
+	private TextView s_ek9;
+	private View rectangle_25_ek8;
+	private TextView s_ek10;
+	private View rectangle_25_ek9;
+	private TextView s_ek11;
+	private View ellipse_12_ek2;
+	private ImageView vector_9;
+	private TextView s_ek12;
+	private View rectangle_24_ek3;
+	private View ellipse_12_ek3;
+	private View rectangle_25_ek10;
+	private TextView s_ek13;
+	private View rectangle_25_ek11;
+	private TextView s_ek14;
+	private TextView wednesday_ek1;
+	private TextView s_ek15;
+	private View rectangle_25_ek12;
+	private TextView s_ek16;
+	private View days_button_ek14;
+	private TextView s_ek17;
+	private ImageView vector_9_ek1;
+	private ImageView home_button;
+
+	//add the variable to hold each time_block
+	private RelativeLayout time_block_1;
+	private RelativeLayout time_block_2;
+	private RelativeLayout time_block_3;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.schedule);
+
+
+		_bg__schedule_ek2 = (View) findViewById(R.id._bg__schedule_ek2);
+		image_bg = (ImageView) findViewById(R.id.image_bg);
+		schedule_box = (ImageView) findViewById(R.id.schedule_box);
+		_10_30am = (TextView) findViewById(R.id._10_30am);
+		_10_30am_ek1 = (TextView) findViewById(R.id._10_30am_ek1);
+		_307692789_491831092502669_2658396862767040473_n_1 = (ImageView) findViewById(R.id._307692789_491831092502669_2658396862767040473_n_1);
+		question_button = (TextView) findViewById(R.id.question_button);
+		add_button = (TextView) findViewById(R.id.add_button);
+		handler_1 = (View) findViewById(R.id.handler_1);
+		wrap_Monday = (View) findViewById(R.id.wrap_Monday);
+		s = (TextView) findViewById(R.id.s);
+		rectangle_25_ek1 = (View) findViewById(R.id.rectangle_25_ek1);
+		rectangle_25_ek2 = (View) findViewById(R.id.rectangle_25_ek2);
+		s_ek1 = (TextView) findViewById(R.id.s_ek1);
+		s_ek2 = (TextView) findViewById(R.id.s_ek2);
+		s_ek3 = (TextView) findViewById(R.id.s_ek3);
+		_10_30am_ek2 = (TextView) findViewById(R.id._10_30am_ek2);
+		s_ek4 = (TextView) findViewById(R.id.s_ek4);
+		s_ek5 = (TextView) findViewById(R.id.s_ek5);
+		ellipse_12 = (View) findViewById(R.id.ellipse_12);
+		ellipse_12_ek1 = (View) findViewById(R.id.ellipse_12_ek1);
+		on_off_1 = (View) findViewById(R.id.on_off_1);
+		rectangle_24_ek2 = (View) findViewById(R.id.rectangle_24_ek2);
+		rectangle_25_ek4 = (View) findViewById(R.id.rectangle_25_ek4);
+		s_ek6 = (TextView) findViewById(R.id.s_ek6);
+		rectangle_25_ek5 = (View) findViewById(R.id.rectangle_25_ek5);
+		s_ek7 = (TextView) findViewById(R.id.s_ek7);
+		rectangle_25_ek6 = (View) findViewById(R.id.rectangle_25_ek6);
+		rectangle_25_ek7 = (View) findViewById(R.id.rectangle_25_ek7);
+		s_ek8 = (TextView) findViewById(R.id.s_ek8);
+		s_ek9 = (TextView) findViewById(R.id.s_ek9);
+		rectangle_25_ek8 = (View) findViewById(R.id.rectangle_25_ek8);
+		s_ek10 = (TextView) findViewById(R.id.s_ek10);
+		rectangle_25_ek9 = (View) findViewById(R.id.rectangle_25_ek9);
+		s_ek11 = (TextView) findViewById(R.id.s_ek11);
+		ellipse_12_ek2 = (View) findViewById(R.id.ellipse_12_ek2);
+		vector_9 = (ImageView) findViewById(R.id.vector_9);
+		s_ek12 = (TextView) findViewById(R.id.s_ek12);
+		rectangle_24_ek3 = (View) findViewById(R.id.rectangle_24_ek3);
+		ellipse_12_ek3 = (View) findViewById(R.id.ellipse_12_ek3);
+		rectangle_25_ek10 = (View) findViewById(R.id.rectangle_25_ek10);
+		s_ek13 = (TextView) findViewById(R.id.s_ek13);
+		rectangle_25_ek11 = (View) findViewById(R.id.rectangle_25_ek11);
+		s_ek14 = (TextView) findViewById(R.id.s_ek14);
+		wednesday_ek1 = (TextView) findViewById(R.id.wednesday_ek1);
+		s_ek15 = (TextView) findViewById(R.id.s_ek15);
+		rectangle_25_ek12 = (View) findViewById(R.id.rectangle_25_ek12);
+		s_ek16 = (TextView) findViewById(R.id.s_ek16);
+		days_button_ek14 = (View) findViewById(R.id.days_button_ek14);
+		s_ek17 = (TextView) findViewById(R.id.s_ek17);
+		vector_9_ek1 = (ImageView) findViewById(R.id.vector_9_ek1);
+		home_button = (ImageView) findViewById(R.id.home_button);
+		//add 3 time block variables
+		time_block_1 = (RelativeLayout) findViewById(R.id.time_block_1);
+		time_block_2 = (RelativeLayout) findViewById(R.id.time_block_2);
+		time_block_3 = (RelativeLayout) findViewById(R.id.time_block_3);
+
+		home_button.setOnClickListener(new View.OnClickListener() {
+		
+			public void onClick(View v) {
+				
+				Intent nextScreen = new Intent(getApplicationContext(), main_activity.class);
+				startActivity(nextScreen);
+			
+		
+			}
+		});
+		
+
+		//when click time_block: move to the pick_hours page
+		time_block_1.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				Intent nextScreen = new Intent(getApplicationContext(), pick_hours_activity.class);
+				startActivity(nextScreen);
+
+			}
+		});
+
+		time_block_2.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+
+				Intent nextScreen = new Intent(getApplicationContext(), pick_hours_activity.class);
+				startActivity(nextScreen);
+
+			}
+		});
+
+		time_block_3.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+
+				Intent nextScreen = new Intent(getApplicationContext(), pick_hours_activity.class);
+				startActivity(nextScreen);
+
+			}
+		});
+
+
+	}
+}
+	
+	
