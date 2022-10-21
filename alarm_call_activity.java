8d113659c0109e93cc0411970597923f5d0d6b4b
Mai Bui <83102564+MaiBui-code@users.noreply.github.com>
2022-10-21 00:45:52 -0400

    Add files via upload

---
+++ alarm_call_activity.java
@@ -0,0 +1,56 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		schedule
+	 *	@date 		Monday 10th of October 2022 08:32:53 PM
+	 *	@title 		Sleep (Screens)
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class alarm_call_activity extends Activity {
+
+	
+	private View _bg__alarm_call_ek2;
+	private ImageView image_7;
+	private TextView sweetie;
+	private ImageView screen_shot_2022_09_22_at_9_15_1;
+	private ImageView screen_shot_2022_09_22_at_9_17_1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.alarm_call);
+
+		
+		_bg__alarm_call_ek2 = (View) findViewById(R.id._bg__alarm_call_ek2);
+		image_7 = (ImageView) findViewById(R.id.image_7);
+		sweetie = (TextView) findViewById(R.id.sweetie);
+		screen_shot_2022_09_22_at_9_15_1 = (ImageView) findViewById(R.id.screen_shot_2022_09_22_at_9_15_1);
+		screen_shot_2022_09_22_at_9_17_1 = (ImageView) findViewById(R.id.screen_shot_2022_09_22_at_9_17_1);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
